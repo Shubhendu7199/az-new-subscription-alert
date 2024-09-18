864d5fc4e1d317a1254ba084338b25d4cf12141f
ZarakiKenpachi7 <shubhendu.upadhyay1@wpp.com>
2024-09-18 12:20:37 +0530

    AzSubs

--- scripts/subscription_scan.ps1
+++ scripts/subscription_scan.ps1
@@ -102,108 +102,24 @@ if (Test-Path $fileYesterday) {
         $body = @{
             "@type" = "MessageCard"
             "@context" = "http://schema.org/extensions"
-            "themeColor" = "0076D7"
-            "summary" = "Larry Bryant created a new task"
+            "themeColor" = "0078D7"
+            "summary" = "New Azure Subscriptions Found"
+            "title" = "New Azure Subscriptions Detected"
             "sections" = @(
                 @{
-                    "activityTitle" = "Larry Bryant created a new task"
-                    "activitySubtitle" = "On Project Tango"
-                    "activityImage" = "https://adaptivecards.io/content/cats/3.png"
-                    "facts" = @(
-                        @{
-                            "name" = "Assigned to"
-                            "value" = "Unassigned"
-                        },
-                        @{
-                            "name" = "Due date"
-                            "value" = "Mon May 01 2017 17:07:18 GMT-0700 (Pacific Daylight Time)"
-                        },
-                        @{
-                            "name" = "Status"
-                            "value" = "Not started"
-                        }
-                    )
+                    "activityTitle" = "Detected new Azure subscriptions"
+                    "facts" = $subscriptionsFormatted
                     "markdown" = $true
                 }
             )
             "potentialAction" = @(
                 @{
-                    "@type" = "ActionCard"
-                    "name" = "Add a comment"
-                    "inputs" = @(
-                        @{
-                            "@type" = "TextInput"
-                            "id" = "comment"
-                            "isMultiline" = $false
-                            "title" = "Add a comment here for this task"
-                        }
-                    )
-                    "actions" = @(
-                        @{
-                            "@type" = "HttpPOST"
-                            "name" = "Add comment"
-                            "target" = "https://learn.microsoft.com/outlook/actionable-messages"
-                        }
-                    )
-                },
-                @{
-                    "@type" = "ActionCard"
-                    "name" = "Set due date"
-                    "inputs" = @(
-                        @{
-                            "@type" = "DateInput"
-                            "id" = "dueDate"
-                            "title" = "Enter a due date for this task"
-                        }
-                    )
-                    "actions" = @(
-                        @{
-                            "@type" = "HttpPOST"
-                            "name" = "Save"
-                            "target" = "https://learn.microsoft.com/outlook/actionable-messages"
-                        }
-                    )
-                },
-                @{
                     "@type" = "OpenUri"
-                    "name" = "Learn More"
+                    "name" = "View Subscriptions"
                     "targets" = @(
                         @{
                             "os" = "default"
-                            "uri" = "https://learn.microsoft.com/outlook/actionable-messages"
-                        }
-                    )
-                },
-                @{
-                    "@type" = "ActionCard"
-                    "name" = "Change status"
-                    "inputs" = @(
-                        @{
-                            "@type" = "MultichoiceInput"
-                            "id" = "list"
-                            "title" = "Select a status"
-                            "isMultiSelect" = $false
-                            "choices" = @(
-                                @{
-                                    "display" = "In Progress"
-                                    "value" = "1"
-                                },
-                                @{
-                                    "display" = "Active"
-                                    "value" = "2"
-                                },
-                                @{
-                                    "display" = "Closed"
-                                    "value" = "3"
-                                }
-                            )
-                        }
-                    )
-                    "actions" = @(
-                        @{
-                            "@type" = "HttpPOST"
-                            "name" = "Save"
-                            "target" = "https://learn.microsoft.com/outlook/actionable-messages"
+                            "uri" = "https://portal.azure.com/"
                         }
                     )
                 }
